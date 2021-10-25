e601e494d4ee380dc8a374591e3afe5510cd8f4a
AntonioOrtega89 <82838094+AntonioOrtega89@users.noreply.github.com>
2021-10-24 19:20:09 -0500

    Add files via upload

---
+++ EVA1_2_STACK/src/eva1_2_stack/EVA1_2_STACK.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_2_stack;
+
+/**
+ *
+ * @author JOSE ALBERTO PEREZ
+ */
+public class EVA1_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("INICIA MAIN");
+        A();
+        System.out.println("TERMINA MAIN");
+
+    }
+    public static void A(){
+         System.out.println("INICIA MAIN");
+         B();
+         System.out.println("TERMINA MAIN");
+
+    }
+
+    public static void B(){
+        System.out.println("INICIA MAIN");
+        int i = 0;
+        double d = 0.0;
+        System.out.println("TERMINA MAIN");
+    }
+    
+}
